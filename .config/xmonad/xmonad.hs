ab62e7d2335d6cd8519c66d95f9db7da9751ef15
goku <ayushkunwar167@gmail.com>
2023-06-04 13:36:53 +0530

    added new keybindings

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -4,12 +4,15 @@
 
 -- Base
 import XMonad
+import System.Directory
 
 -- Data
 import Data.Monoid
 
 -- Utilities
 import XMonad.Util.EZConfig
+import XMonad.Util.Run (runProcessWithInput, safeSpawn, spawnPipe)
+import XMonad.Util.SpawnOnce
 
 -- Hooks
 import XMonad.Hooks.ManageDocks
@@ -48,6 +51,9 @@ myNormalBorderColor  = "#99918A"
 --myFocusedBorderColor = "#ff0000"
 myFocusedBorderColor = "#8be9fd"
 
+mySoundPlayer :: String
+mySoundPlayer = "ffplay -nodisp -autoexit " -- The program that will play system sounds
+
 myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
     -- mod-button1, Set the window to floating mode and move by dragging
     [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w
@@ -68,17 +74,31 @@ myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
      ratio   = 1/2
      delta   = 3/100
 
+-- Use xprop to get wm name
+
 myManageHook = composeAll
     [ className =? "Picture-in-Picture" --> doFloat
     , className =? "firefox"         --> doShift "1"
-    , className =? "spotify"        --> doShift "6"
+    , className =? "Spotify"        --> doShift "6"
     , className =? "Gimp"           --> doFloat
     , resource  =? "desktop_window" --> doIgnore
     , resource  =? "kdesktop"       --> doIgnore ]
 
 myEventHook = mempty
 myLogHook = return ()
-myStartupHook = return ()
+
+myStartupHook :: X ()
+myStartupHook = do
+  spawnOnce (mySoundPlayer ++ startupSound)
+
+soundDir = "/opt/dtos-sounds/" -- The directory that has the sound files
+
+startupSound  = soundDir ++ "startup-01.mp3"
+shutdownSound = soundDir ++ "shutdown-01.mp3"
+dmenuSound    = soundDir ++ "menu-01.mp3"
+menuSound     = soundDir ++ "bubblebeam.mp3"
+sagSound      = soundDir ++ "sab.mp3"
+emailSound    = soundDir ++ "the-notification-email.mp3"
 
 main :: IO ()
 main = xmonad $ def
@@ -102,9 +122,9 @@ main = xmonad $ def
     }
   `additionalKeysP`
     [ ("M-<Return>", spawn "alacritty"),
-      ("M-d", spawn "dmenu_run"),
+      ("M-d" ,sequence_ [spawn (mySoundPlayer ++ dmenuSound), spawn "dmenu_run"]),
       ("M-S-z", spawn "flameshot gui"),
-      ("M-S-q", kill),
+      ("M-S-q",sequence_ [spawn (mySoundPlayer ++ sagSound), kill]),
       ("M-S-<Down>", spawn "brightnessctl set 355-"),
       ("M-S-<Up>", spawn "brightnessctl set 355+"),
       ("M-<Space>", sendMessage NextLayout),
@@ -114,6 +134,6 @@ main = xmonad $ def
       ("M-j", windows W.focusDown),
       ("M-k", windows W.focusUp),
       ("M-m", windows W.focusMaster),
-      ("M-S-o", io (exitWith ExitSuccess))
+      ("M-S-o",sequence_ [spawn (mySoundPlayer ++ shutdownSound), io (exitWith ExitSuccess)])
       
     ]
