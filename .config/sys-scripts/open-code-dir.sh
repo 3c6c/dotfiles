76523b31843c7d0361714e689be67f413e079114
goku <ayushkunwar167@gmail.com>
2023-06-04 13:40:56 +0530

    Opening code Directory

---
+++ .config/sys-scripts/open-code-dir.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+codelocation=~/Documents/codeplayground/
+
+function main() {
+ echo "Enter a directory from below:"
+ ls $codelocation
+ read choice
+ cd $codelocation"$choice"
+}
+main
